c9704e03c0cfe8e8e6d368e044f3eba3f53ab321
Az4reus <az4reus@gmail.com>
2017-05-15 06:57:44 +0200

    Adds Terminal spawning

---
+++ init.lua
@@ -0,0 +1,28 @@
+hs.hotkey.bind({"ctrl", "alt", "cmd"}, "H", function()
+    hs.notify.new({title="Welcome to hammerspoon and such",
+        informativeText="Hello World and all that shit"}):send()
+end)
+
+hs.hotkey.bind({"shift", "ctrl"}, "R", function()
+    hs.reload()
+    hs.notify.new({title="Config reloaded"}):send()
+end)
+
+-- Iterm2 automation
+
+hs.hotkey.bind({"shift", "cmd"}, "S", function()
+    local spawnNewTerminal = [[
+        tell application "iTerm2"
+            create window with default profile
+            tell current session in current window
+                set columns to 80
+                set rows to 25
+            end tell
+        end tell
+    ]]
+    local success, object, description = hs.osascript.applescript(spawnNewTerminal)
+    if not success then
+        hs.notify.new({title="Spawning new Terminal not successful :(",
+            informativeText="Error message: " .. description}):send()
+    end
+end)
