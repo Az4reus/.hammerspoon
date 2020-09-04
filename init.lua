12de46fbccd609ce040a8c40842d3f1b7a06bc22
Mordecai Malignatus <az4reus@gmail.com>
2020-09-04 09:54:58 -0500

    Change: iTerm2 -> Terminal.app

--- init.lua
+++ init.lua
@@ -1,18 +1,19 @@
+-- Reload config any time. 
 hs.hotkey.bind({"shift", "ctrl"}, "R", function()
     hs.reload()
     hs.notify.new({title="Config reloaded"}):send()
 end)
 
--- Iterm2 automation
-
+-- Terminal automation
 hs.hotkey.bind({"shift", "ctrl"}, "D", function()
     local spawnNewTerminal = [[
-        tell application "iTerm2"
-            create window with default profile
-            tell current session in current window
-                set columns to 80
-                set rows to 25
-            end tell
+        tell application "Terminal"
+                if (exists window 1) then
+                        activate
+                else
+                        do script ""
+                        activate
+                end
         end tell
     ]]
     local success, object, description = hs.osascript.applescript(spawnNewTerminal)
